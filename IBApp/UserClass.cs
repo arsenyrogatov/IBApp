25e3f1f17bc1678e8018310812c4203cf66d3617
arsenyrogatov <arsenyrogatov@gmail.com>
2023-02-23 12:27:52 +0300

    Добавьте файлы проекта.

---
+++ IBApp/UserClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBApp
+{
+    internal static class UserClass
+    {
+        public static string Login { get; set; }
+
+        public static int TryLogin (string login, string pass)
+        {
+
+        }
+    }
+}
